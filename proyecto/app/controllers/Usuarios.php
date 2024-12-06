83c765f977cfc9561fd559f24d1e638406977cd9
AracelyGlez <aracelyglezsc4@gmail.com>
2024-12-06 15:01:38 -0600

    base

--- proyecto/app/controllers/Usuarios.php
+++ proyecto/app/controllers/Usuarios.php
@@ -2,6 +2,13 @@
 class Usuarios extends Controller
 {
     // inicializar un modelo
+    private $usuariosModel;
+
+    public function __construct()
+    {
+        // construct para usar la conexion a base de datos y creacion de modelo
+        $this->usuariosModel = $this->model('Usuario');
+    }
     public function vacaciones() {
         $this->view('usuarios/vacaciones');
     }
---
+++ proyecto/app/models/Usuario.php
@@ -0,0 +1,10 @@
+<?php
+
+class Usuario
+{
+    private $db;
+    public function __construct()
+    {
+        $this->db = new Base;
+    }
+}
