4654ae20291b28c746e75a73dc1245db4b24a8c0
SaidaGutierrez28 <L21540356@delicias.tecnm.mx>
2024-12-06 18:55:58 -0600

    s1

--- proyecto/app/views/Usuarios/vacaciones.php
+++ proyecto/app/views/Usuarios/vacaciones.php
@@ -5,7 +5,7 @@ include APPROOT .'/views/includes/encabezado.inc.php';
   
         <!-- Título del formulario -->
         <div class="p-3 mb-3 text-center">
-            <h2 class="mb-0">Formulario de Solicitud de Tiempo Vacacional</h2>
+            <h2 class="mb-0">Nominas en Tiempo Vacacional</h2>
         </div>
 
         <form
